6e704434ed0ed5ef5750383d81b67911b4d5b77e
Kredi <luca.humke@web.de>
2019-05-04 21:32:54 +0200

    Edited wahl view

--- projectwoche/projektwahl/views.py
+++ projectwoche/projektwahl/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Projekt, Schueler
 
 
 def Wahl(request):
-    return HttpResponse(content="Hier wird gewählt")
+    if(request.Type == "POST"):
+        try:
+            schueler = Schueler.objects.get(name='request.POST.get("name")')
+        except Exception as e:
+            return render(request, 'projektwahl/projektwahl.html', context={'project': project,
+                                                                            'status': "Schüler wurde nicht gefunden"})
+
+
+
+    #Standart Site
+    project = Projekt.objects.all()
+    return render(request, 'projektwahl/projektwahl.html', context={'project': project,
+                                                                    'status': ""})
 # Create your views here.
